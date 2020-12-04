0391687c52c34eaff915d03f1db25a67851e5aac
Crowdbotics <team@crowdbotics.com>
2020-12-04 13:40:40 +0000

    Update components

--- src/features/BlankScreen015463/screens/blank.js
+++ src/features/BlankScreen015463/screens/blank.js
@@ -23,13 +23,39 @@ export default class Blank extends React.Component {
     }
   }
 
-  state = {}
+  state = { CheckBox_3: true, CheckBox_4: true }
 
   render = () => (
     <View>
       <Text>Sample text content</Text>
+      <CheckBox
+        title="Radio button"
+        checkedIcon="dot-circle-o"
+        uncheckedIcon="circle-o"
+        checked={this.state.CheckBox_3}
+        onPress={nextChecked => this.setState({ CheckBox_3: nextChecked })}
+      />
+      <CheckBox
+        title="Checkbox"
+        checked={this.state.CheckBox_4}
+        onPress={nextChecked => this.setState({ CheckBox_4: nextChecked })}
+      />
+      <Slider
+        value={50}
+        minimumValue={0}
+        maximumValue={100}
+        style={styles.Slider_6}
+      />
+      <Button title="Press me!" onPress={() => alert("Pressed!")} />
     </View>
   )
 }
 
-const styles = StyleSheet.create({ View_1: {}, Text_2: {} })
+const styles = StyleSheet.create({
+  View_1: {},
+  Text_2: {},
+  CheckBox_3: {},
+  CheckBox_4: {},
+  Slider_6: { width: 100 },
+  Button_5: {}
+})
